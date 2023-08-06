06b2220926eb77e4a47b19a79ed4d4794a78b3b9
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2023-08-06 21:25:39 +0700

    Create gojek_icons model for preparing data that needed

---
+++ lib/models/gojek_icons.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class GojekIcons {
+  final String icon;
+  final String title;
+  final Color color;
+
+  GojekIcons(this.icon, this.title, this.color);
+}
